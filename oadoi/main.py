4f49f27ba49de2a430b76de37c32c06886a0c97e
Bijan Ranjbar <bij.ranjbar@gmail.com>
2017-06-19 10:28:15 +0200

    Update main.py

--- oadoi/main.py
+++ oadoi/main.py
@@ -2,43 +2,43 @@
 a sample api request is as following
 https://api.oadoi.org/10.1038/nature12373?email=b.ranjbarsahraei@tudelft.nl
 
+
+the input to this code is a dois.txt file that contains one doi per line.
 """
 
 import urllib
-
 import json
 
-import simplejson as simplejson
+FOLDER = ""
 
+thread_number = 0
+batch_size = 100000  
+# please note that the batch_size limit can be increased if you have more than 100,000 dois in your file, 
+# or alternatively you can run the file in multiple iterations by increasing the thread_number.
 
-FOLDER = "DOIs/"
+f = "dois.txt"
+print f
+file = FOLDER + f
+f_output = open(FOLDER + f.replace('.txt',str(thread_number)) + "_enriched.txt", "w")
+with open(file, "r") as f:
 
-from os import listdir
-from os.path import isfile, join
-onlyfiles = [f for f in listdir(FOLDER) if isfile(join(FOLDER, f)) and ".txt" in f]
-onlyfiles = [onlyfiles[12]]
-for f in onlyfiles:
-    print f
-    file = FOLDER + f
-    f_output = open(FOLDER + f + "_enriched.txt", "w")
-    with open(file, "r") as f:
-
-        # making the header
-        doi = "10.1038/nature12373"
-        requestURL = "http://api.oadoi.org/%s?email=b.ranjbarsahraei@tudelft.nl" % doi
-        response = urllib.urlopen(requestURL).read()
-        results = simplejson.loads((response))["results"][0]
-        header = results.keys()
-        f_output.write('\t'.join(header) + '\n')
-        counter = 0
-        all_lines = f.readlines()
-        for i, l in enumerate(all_lines):
-            print i , 'out of ', len(all_lines)
-            doi = l
-            if doi:
-                row = ''
-                requestURL = "http://api.oadoi.org/%s?email=b.ranjbarsahraei@tudelft.nl" % doi
-                response = simplejson.loads((urllib.urlopen(requestURL).read()))
+    # making the header
+    doi = "10.1038/nature12373"
+    requestURL = "http://api.oadoi.org/%s?email=b.ranjbarsahraei@tudelft.nl" % doi
+    response = urllib.urlopen(requestURL).read()
+    results = json.loads((response))["results"][0]
+    header = results.keys()
+    f_output.write('\t'.join(header) + '\n')
+    counter = 0
+    all_lines = f.readlines()[thread_number*batch_size: (thread_number+1)*batch_size]
+    for i, l in enumerate(all_lines):
+        print i + (thread_number*batch_size) , 'out of ', (thread_number+1)*batch_size
+        doi = l.replace('\n','')
+        if doi:
+            row = '%s\t' % doi
+            requestURL = "http://api.oadoi.org/%s?email=b.ranjbarsahraei@cwts.leidenuniv.nl" % doi
+            try:
+                response = json.loads((urllib.urlopen(requestURL).read()))
                 if "results" in response.keys():
                     results = response["results"][0]
                     for j, h in enumerate(header):
@@ -48,9 +48,8 @@ for f in onlyfiles:
                                 content += r.encode('utf-8') + ';'
                         if isinstance(results[h], int):
                             content = str(results[h])
-                        if isinstance(results[h], str):
+                        if isinstance(results[h], basestring):
                                 content = results[h].encode('utf-8')
-
                         row += content
                         if j == len(header) - 1:
                             row +=  '\n'
@@ -58,7 +57,12 @@ for f in onlyfiles:
                             row += '\t'
                     f_output.write(row)
                 else:
-                    f_output.write('\n')
-            else:
-                f_output.write('\n')
+                    f_output.write(doi + '\n')
+            except:
+                f_output.write(doi + '\n')
+                print 'error with doi' + doi
+                print requestURL
 
+                        
+        else:
+            f_output.write('\n')
